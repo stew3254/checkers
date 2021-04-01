376f84b9673832f905329d94239b971f6d919b39
stew3254 <rtstewy@gmail.com>
2021-04-01 17:08:04 -0400

    Add pug and sass templating to jinja

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+import flask
+import jinja2
+from sassutils.wsgi import SassMiddleware
 
-app = Flask(__name__)
+# Create the flask app
+app = flask.Flask(__name__)
+# Allow using pug for templating
+app.jinja_loader = jinja2.FileSystemLoader("templates/pug")
+app.jinja_env.add_extension("pypugjs.ext.jinja.PyPugJSExtension")
+# Allow sass to be used instead of css
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'app': {'sass_path': 'static/sass',
+            'css_path': 'static/css',
+            'strip_extension': True,
+            },
+})
 
 
-@app.route('/')
-def hello_world():
-    return 'Bet did things!'
+@app.route("/", methods=["GET"])
+def index():
+    return flask.render_template("index.pug")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
