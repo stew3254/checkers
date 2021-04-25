afd39ce195e1d37bfb9004d3a5bcea9a4fd30732
stew3254 <rtstewy@gmail.com>
2021-04-25 19:45:16 -0400

    Can now place moves regularly by selecting a piece and then a tile

--- src/static/js/checkers.js
+++ src/static/js/checkers.js
@@ -1,5 +1,5 @@
 const BASE_URL = window.location.href;
-let selected_piece = null;
+let selectedPiece = null;
 
 // Gets a cookie
 const getCookie = (cookieKey) => {
@@ -20,6 +20,20 @@ function convertPosition(pos) {
 	let col = letters.indexOf(pos[0]);
 	let row = Number(pos[1]) - 1;
 	return {column: col, row: row};
+}
+
+function getPosition(e) {
+	let id = null;
+	// Check if it's a piece or a tile
+	if (e.hasClass("piece")) {
+		id = e.parent().attr("id");
+	} else if (e.hasClass("board_tile")) {
+		id = e.attr("id");
+	} else {
+		return {row: -1, column: -1}
+	}
+	// Return the position of the tile
+	return convertPosition(id.slice(id.length - 2));
 }
 
 function post(params, path) {
@@ -74,19 +88,38 @@ function makeJump(piece, position, end_turn) {
 function selectPiece(piece) {
 	if (piece.hasClass("player_piece")) {
 		// Update global piece
-		if (selected_piece != null)
-			selected_piece.css({border: "none"});
-		selected_piece = piece;
+		if (selectedPiece != null)
+			selectedPiece.css({border: "none"});
+		selectedPiece = piece;
 		piece.css({border: ".4rem solid white"});
 	} else {
-		if (selected_piece != null) {
-			let id = piece.parent().attr("id");
-			let ai_pos = convertPosition(id.slice(id.length - 2));
-			id = selected_piece.parent().attr("id")
-			let pos = convertPosition(id.slice(id.length - 2));
+		// Trying to select an ai piece
+		if (selectedPiece != null) {
+			let aiPos = getPosition(piece);
+			let pos = getPosition(selectedPiece);
 			// Try to jump this piece
-			if (Math.abs(pos.row - ai_pos.row) === 1) {
-				makeJump(pos, ai_pos, false);
+			if (Math.abs(pos.row - aiPos.row) === 1) {
+				makeJump(pos, aiPos, false);
+			}
+		}
+	}
+}
+
+function selectTile(tile) {
+	// See if a piece is already selected
+	if (selectedPiece != null) {
+		let id = tile.attr("id");
+		let pos = convertPosition(id.slice(id.length - 2));
+
+		// See if a piece already exists on this tile
+		let children = tile.children();
+		// No pieces on this tile
+		if (children.length === 0) {
+			let piecePos = getPosition(selectedPiece);
+			console.log(piecePos, pos);
+			// See if the tile is next to it
+			if (Math.abs(piecePos.row - pos.row) === 1) {
+				makeMove(piecePos, pos);
 			}
 		}
 	}
@@ -98,4 +131,12 @@ let pieces = $(".piece");
 // Register onclick handlers
 pieces.each((index, piece) => {
 	piece.onclick = () => selectPiece($(piece));
+});
+
+// Get all tiles
+let tiles = $(".board_tile.black");
+
+// Register onclick handlers
+tiles.each((index, tile) => {
+	tile.onclick = () => selectTile($(tile));
 });
