8fff5bb8c409a1b177d9f132dcf679c1da415186
stew3254 <rtstewy@gmail.com>
2021-04-25 14:51:39 -0400

    Add game id to ai

--- src/ai.py
+++ src/ai.py
@@ -1,8 +1,12 @@
 import checkers
+from database import db
+
+session = db.session
 
 
 class AI:
-    def __init__(self):
+    def __init__(self, game_id: int):
+        self.game_id = game_id
         pass
 
     def heurisitc(self):
