e451270c2f6c65d95ccd6d5754c37e23aac145f6
Elias <besculek@clarkson.edu>
2021-04-25 20:37:58 -0400

    changes to AI.py

--- src/ai.py
+++ src/ai.py
@@ -11,11 +11,20 @@ class AI:
         self.game_id = game_id
         pass
 
-    def heurisitc(self):
-        pass
+    def get_heurisitc(self):
+        heuristic = 0
+
+        return heuristic
+        #pass
 
     def make_move(self):
         # This can be a checkers.make_jump() or checkers.make_move()
+        session_state = checkers.check_game_state(session, id)
+        pieces = checkers.board_state(session_state)
+        for piece in pieces:
+            moves = checkers.get_moves(session_state, piece)
+
+
         pass
 
     def evaluate(self):
