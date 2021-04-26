a6d929d42952b232413c6e778ac1ef9c289a2954
Elias <besculek@clarkson.edu>
2021-04-25 21:29:45 -0400

    changes to AI.py

--- src/ai.py
+++ src/ai.py
@@ -11,21 +11,25 @@ class AI:
         self.game_id = game_id
         pass
 
-    def get_heurisitc(self):
+    def get_heurisitc(moves):
         heuristic = 0
+        if moves == []:
+            return heuristic
+        else:
 
         return heuristic
         #pass
 
     def make_move(self):
         # This can be a checkers.make_jump() or checkers.make_move()
-        session_state = checkers.check_game_state(session, id)
-        pieces = checkers.board_state(session_state)
+        session_state = checkers.check_game_state(session, self.game_id)
+        pieces = checkers.board_state(session_state, self.game_id)
         for piece in pieces:
-            moves = checkers.get_moves(session_state, piece)
-
-
+            moves = checkers.get_moves(session, self.game_id, piece)
+            self.get_heuristic(moves)
+            print(moves)
         pass
 
     def evaluate(self):
         pass
+
