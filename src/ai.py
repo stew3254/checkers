3d3787f52723bb6a6a7efb433e6a5d5a90835a20
Elias <besculek@clarkson.edu>
2021-04-26 22:42:56 -0400

    10:42 commit 4/26

--- src/ai.py
+++ src/ai.py
@@ -12,32 +12,57 @@ class AI:
         pass
 
     def get_heurisitc(moves):
-        move_heuristic = []
+        moves_heuristic = []
         if moves == []:
-            move_heuristic.append(0)
+            moves_heuristic.append(0)
         elif moves != []:
-            for move in moves:
-                #possible move heuristics
-                #can be jumped by opponent lower heuristic
-                #closer to becoming a king higher heuristic
-                #cant be jumped by opponent higher hearistic
-                move_heuristic.append(5)
+            for action in moves:
+                moves_heuristic.append(5)
+        i = 0
+        average = 0
+        for action in moves_heuristic:
+            average = average + action
+            i = i + 1
+        average = average/i
 
-        return move_heuristic
+        return (average, moves_heuristic)
         #pass
+
+
 
     def make_move(self):
         # This can be a checkers.make_jump() or checkers.make_move()
         piece_heuristics = []
         session_state = checkers.check_game_state(session, self.game_id)
         pieces = checkers.board_state(session_state, self.game_id)
+
         for piece in pieces:
             moves = checkers.get_moves(session, self.game_id, piece)
             piece_heuristics.append(self.get_heuristic(moves))
             print(moves)
             print(piece_heuristics)
+
+        highest = 0
+        highest_piece = piece_heuristics[0]
+        for piece in piece_heuristics:
+            if piece[0] > highest:
+                highest = piece[0]
+                highest_piece = piece
+        highest_move = 0
+        for moves in highest_piece[1]:
+            for move in moves:
+                if move > highest_move:
+                    highest_move = move
+
+        #highest piece is the piece the AI needs to Move
+        #Highest_move is where the piece moves to
+        print("HERE")
+        print(highest_piece)
+        print(highest_move)
+
         pass
 
     def evaluate(self):
+        self.make_move()
         pass
 
